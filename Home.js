f2e4d1f730e869302d20587d15f7a44d5fa8d565
Alexander Burdiss <aburdiss@icloud.com>
2023-02-17 21:43:08 -0500

    Add Event functionality to Homepage

--- Home.js
+++ Home.js
@@ -1,15 +1,25 @@
 let PROJECT_ID = '9qmqbv2y';
 let DATASET = 'production';
-let QUERY = encodeURIComponent(`*[_type == "home"]{
-  headline,
-  "imageUrl": mainImage.asset->url,
-  "imageAlt": mainImage.alt,
-  firstSectionHeadline,
-  firstSectionText,
-  donateHeadline,
-  secondSectionText,
-  eventsHeadline,
-  noEventsText,
+let QUERY = encodeURIComponent(`*[_type in ["home", "event"]]{
+  _type == "home" => {
+    headline,
+    "imageUrl": mainImage.asset->url,
+    "imageAlt": mainImage.alt,
+    firstSectionHeadline,
+    firstSectionText,
+    donateHeadline,
+    secondSectionText,
+    eventsHeadline,
+    noEventsText,
+  },
+  _type == "event" => {
+    headline,
+    date,
+    address1,
+    address2,
+    address3,
+    "imageUrl": image.asset->url
+  }
 }`);
 
 let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
@@ -26,14 +36,29 @@ const firstSectionTextContainer = document.querySelector('#first-section-text');
 const donateHeadlineContainer = document.querySelector('#donate-headline');
 
 const eventsHeadlineContainer = document.querySelector('#events-headline');
+const eventsContainer = document.querySelector('#events-container');
 const noEventsTextContainer = document.querySelector('#no-events-text');
+
+const dateOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+
+const timeOptions = {
+  hour: 'numeric',
+  minute: '2-digit',
+};
 
 // fetch the content
 fetch(URL)
   .then((res) => res.json())
   .then(({ result }) => {
-    const page = result[0];
+    const page = result.find((element) => !!element.eventsHeadline);
+    const events = result.filter((element) => !element.eventsHeadline);
 
+    // Handle Page
     const headline = page.headline;
     headlineContainer.innerText = headline;
 
@@ -53,6 +78,34 @@ fetch(URL)
     const eventsHeadline = page.eventsHeadline;
     eventsHeadlineContainer.innerText = eventsHeadline;
 
-    sanityBlockContent(noEventsTextContainer, page.noEventsText);
+    // Handle Events
+    if (events.length) {
+      noEventsTextContainer.remove();
+
+      events.forEach((event) => {
+        const container = document.createElement('div');
+        container.classList.add('upcoming-event');
+        container.style.backgroundImage = `url(${event.imageUrl})`;
+        const markup = `
+<div class="content-box">
+  <h3>${event.headline}</h3>
+  <div class="date">${new Date(event.date).toLocaleDateString(
+    'en-US',
+    dateOptions
+  )}
+  <div>${new Date(event.date).toLocaleTimeString('en-US', timeOptions)}</div>
+  </div>
+  <div class="Address1">${event.address1}</div>
+  <div class="Address2">${event.address2}</div>
+  <div class="Address3">${event.address3}</div>
+</div>`;
+        container.innerHTML = markup;
+        eventsContainer.appendChild(container);
+      });
+    } else {
+      // No events!
+      eventsContainer.remove();
+      sanityBlockContent(noEventsTextContainer, page.noEventsText);
+    }
   })
   .catch((err) => console.error(err));
