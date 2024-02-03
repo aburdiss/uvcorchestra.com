891b95d9553208e994a1d9101a08757fa850e912
Alexander Burdiss <aburdiss@icloud.com>
2024-02-03 07:00:49 -0500

    Update Copyright to update to current year. Move scripts to top of the page with defer attribute

---
+++ assets/js/footer.js
@@ -0,0 +1,3 @@
+const COPYRIGHT = '©' + new Date().getFullYear() + ' Upper Valley Community Orchestra';
+const copyrightContainer = document.querySelector('#copyright');
+copyrightContainer.innerText = COPYRIGHT;
